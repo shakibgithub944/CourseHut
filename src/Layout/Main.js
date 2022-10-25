12fecd739e8b43354debc46b73bea5d2a350061a
Nazmush Shakib <shakibgithub944>
2022-10-25 16:02:46 +0600

    navber added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navber from '../components/Navber';
+import { Outlet } from 'react-router-dom'
 
 const Main = () => {
     return (
         <div>
-            <h1>this is main section</h1>
+            <Navber></Navber>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/Route/Route.js
+++ src/Route/Route.js
@@ -1,10 +1,45 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../components/Blog";
+import Courses from "../components/Courses";
+import Error from "../components/Error";
+import Faq from "../components/Faq";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
 import Main from "../Layout/Main";
 
-export const router = createBrowserRouter([{
-    path: '/',
-    element: <Main></Main>,
-    children: [
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>,
+            },
+            {
+                path: '/Login',
+                element: <Login></Login>,
+            },
+            {
+                path: '/Register',
+                element: <Register></Register>,
+            },
+            {
+                path: '/Course',
+                element: <Courses></Courses>,
+            },
+            {
+                path: '/Blog',
+                element: <Blog></Blog>,
+            },
+            {
+                path: '/Faq',
+                element: <Faq></Faq>,
+            },
+        ]
+    },
 
-    ]
-}])
+
+])
