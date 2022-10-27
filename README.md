6372d6e87ed67ef6e658a2abf32ee1a92cf943b9
Nazmush Shakib <shakibgithub944>
2022-10-27 18:34:47 +0600

    add readme file

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was Tailwind with [Create React App](https://github.com/facebook/create-react-app).
+---
+* Live site link
+[CourseHut](https://coursehut-7e4c9.web.app/)
+---
+* Features and functionality
+This is a digital learning course based website. This websiste is so attractive looking and responsive navber with toggle. So many features on this website.
 
-## Available Scripts
+* Utilities 
+I create this website with react js and I use to tailwind css fremwork and [mambaUi](https://www.mambaui.com/), [kitwind](https://kitwind.io/) and some coustom css.
 
 In the project directory, you can run:
 
-# Here Is Live Site Link
+* Athentication
+This website fully secure and safe Because its used firbase Authenticate .
 
-# Utilities 
+* Theme 
+This website use user friendly theme . So user can comfortably use this website. 
 
-# Descrptions
+
 
--- src/components/Faq.js
+++ src/components/Faq.js
@@ -9,22 +9,21 @@ const Faq = () => {
                     <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
                     <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
                         <details>
-                            <summary className="py-2 outline-none cursor-pointer focus:underline">Optio maiores eligendi molestiae totam dolores similique?</summary>
+                            <summary className="py-2 outline-none cursor-pointer focus:underline">what is c++ used for?</summary>
                             <div className="px-4 pb-4">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde neque in fugiat magni, quas animi enim veritatis deleniti ex. Impedit.</p>
+                                <p>What is C++ Used For? C++ (or “C-plus-plus”) is a general-purpose programming and coding language. C++ is used in developing browsers, operating systems, and applications, as well as in-game programming, software engineering, data structures, etc.</p>
                             </div>
                         </details>
                         <details>
-                            <summary className="py-2 outline-none cursor-pointer focus:underline">Modi dolorem veritatis culpa quos consequuntur beatae itaque excepturi perspiciatis?</summary>
+                            <summary className="py-2 outline-none cursor-pointer focus:underline">what is data science?</summary>
                             <div className="px-4 pb-4">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aspernatur quae, eos explicabo odit minima libero veniam similique quibusdam doloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus?</p>
+                                <p>Data science is an interdisciplinary field that uses scientific methods, processes, algorithms and systems to extract or extrapolate knowledge and insights from noisy, structured and unstructured data, and apply knowledge from data across a broad range of application domains. </p>
                             </div>
                         </details>
                         <details>
-                            <summary className="py-2 outline-none cursor-pointer focus:underline">Magni reprehenderit possimus debitis?</summary>
+                            <summary className="py-2 outline-none cursor-pointer focus:underline">what is artificial intelligence</summary>
                             <div className="px-4 pb-4 space-y-2">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptates aspernatur dolores in consequatur doloremque inventore reprehenderit, consequuntur perspiciatis architecto.</p>
-                                <p>Sed consectetur quod tenetur! Voluptatibus culpa incidunt veritatis velit quasi cupiditate unde eaque! Iure, voluptatibus autem eaque unde possimus quae.</p>
+                                <p>Artificial intelligence is intelligence demonstrated by machines, as opposed to the natural intelligence displayed by animals and humans. </p>
                             </div>
                         </details>
                     </div>
