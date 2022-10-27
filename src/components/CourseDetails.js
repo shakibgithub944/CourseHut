2bcbc0fcafc016d7c1f8f238e9b5a8c628508aff
Nazmush Shakib <shakibgithub944>
2022-10-27 15:54:56 +0600

    Pdf download function implement

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,13 +1,20 @@
-import { StarIcon } from '@heroicons/react/24/solid';
-import React from 'react';
+import { ArrowDownCircleIcon, StarIcon } from '@heroicons/react/24/solid';
+import { React, useRef } from 'react';
 import { useLoaderData, Link } from 'react-router-dom';
+import ReactPrint from 'react-to-print';
 
 const CourseDetails = () => {
     const details = useLoaderData();
+    const ref = useRef();
+
     return (
         <div>
-            <h1 className='text-center  text-5xl font-bold'>{details.title}</h1>
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className='flex justify-center items-center'>
+                <h1 className='mr-5 text-5xl font-bold'>{details.title}</h1>
+                <ReactPrint trigger={() => <span className='text-center mt-4 '><ArrowDownCircleIcon className='h-10 text-gray-500' title='Download Pdf' /></span>} content={() => ref.current} ></ReactPrint>
+            </div>
+
+            <div ref={ref} className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
 
                 <div className="flex flex-col lg:flex-row">
                     <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
